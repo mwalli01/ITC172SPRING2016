ffda4e3e1fe47caf5b8d8af37140f1d6f2e1203d
mwalli01 <maxamaxa@gmail.com>
2016-04-14 11:56:37 -0700

    Update DataClass.cs

--- DataClass.cs
+++ DataClass.cs
@@ -2,16 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-//libraries need to talk to database
 using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
-
 /// <summary>
-/// This class will connect to the database
-/// It will have methods to retrieve the services
-/// It will also retrieve all the grants for that service
-/// John Voorhess 4/12/2016
+/// this class will connect to the database with methods to retrieve services
+/// it will also retrieve all the grants for that service 
 /// </summary>
 public class DataClass
 {
@@ -19,47 +15,42 @@ public class DataClass
     public DataClass()
     {
         connect = new SqlConnection(ConfigurationManager.ConnectionStrings["CommunityAssistConnectionString"].ToString());
-    }//end constructor
+    }
 
     public DataTable GetServices()
     {
         DataTable tbl = null;
-
+        tbl = new DataTable();
         string sql = "Select GrantTypeKey, GrantTypeName from GrantType";
         SqlCommand cmd = new SqlCommand(sql, connect);
-    
-        tbl = ReadData(cmd);
-    
+        tbl = ProcessConnection(cmd);
         return tbl;
     }
 
     public DataTable GetGrants(int grantTypeKey)
     {
         DataTable tbl = null;
-        string sql = "Select GrantRequestDate, GrantRequestExplanation, GrantRequestAmount from GrantRequest Where GrantTypeKey = @Key";
-
+        
+        string sql = "Select GrantRequestDate, GrantRequestExplanation, GrantRequestAmount from GrantRequest from GrantRequest Where GrantTypeKey=@Key";
         SqlCommand cmd = new SqlCommand(sql, connect);
         cmd.Parameters.AddWithValue("@Key", grantTypeKey);
-
-        tbl = ReadData(cmd);
-
+        
+        tbl = ProcessConnection(cmd);
         return tbl;
     }
-
-    private DataTable ReadData(SqlCommand cmd)
+    
+    private DataTable ProcessConnection(SqlCommand cmd)
     {
-        SqlDataReader reader = null;
         DataTable tbl = new DataTable();
-
+        SqlDataReader reader = null;
 
         connect.Open();
+
         reader = cmd.ExecuteReader();
         tbl.Load(reader);
         reader.Close();
         connect.Close();
 
-
         return tbl;
     }
-
-}//end class
+}
