050a01369d82ecd41024b618980c5f185e3489d9
mwalli01 <maxamaxa@gmail.com>
2016-04-14 11:54:57 -0700

    Update Default.aspx.cs

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,17 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Data;//added for DataTable
-public partial class _Default : System.Web.UI.Page //partial class name must match website name. '_' only for default.
-{
+using System.Data;
 
+public partial class _Default : System.Web.UI.Page
+{
     DataClass dc = new DataClass();
     protected void Page_Load(object sender, EventArgs e)
     {
-        if(!IsPostBack)
         LoadDropDownList();
     }
-
     protected void LoadDropDownList()
     {
         DataTable tbl = dc.GetServices();
@@ -22,22 +20,20 @@ public partial class _Default : System.Web.UI.Page //partial class name must mat
         DropDownList1.DataTextField = "GrantTypeName";
         DropDownList1.DataValueField = "GrantTypeKey";
         DropDownList1.DataBind();
-        DropDownList1.Items.Insert(0, "Choose a Service");
+        DropDownList1.Items.Insert(0, "Choose A Service");
     }
 
     protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
     {
+        if(!IsPostBack)
         FillGrid();
     }
 
     protected void FillGrid()
     {
-        if (!DropDownList1.SelectedValue.Equals("Choose a Service") )
-        { 
         int key = int.Parse(DropDownList1.SelectedValue.ToString());
         DataTable tbl = dc.GetGrants(key);
         GridView1.DataSource = tbl;
         GridView1.DataBind();
-        }
     }
 }
